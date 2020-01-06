b9e804c00b6bbaf5c463d6fefc7250adfd0d1818
saif43 <32043353+saif43@users.noreply.github.com>
2020-01-06 17:57:51 +0600

    update

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -3,4 +3,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-// create a react comnonent
+// create a react comnonent 
+
+// function based component
+const App = () => {
+    return (
+        <div>
+            <label for="name">Enter Name</label>
+            <input type="text" name="name" id="name"></input>
+            <button type="submit" style={{backgroundColor:'blue',color:'white'}}>Submit</button>
+        </div>);
+};
+
+// take the react component and show it in browser
+
+ReactDOM.render(<App />, document.querySelector('#root'));
