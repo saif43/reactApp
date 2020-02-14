2b5b86a321dddd5344ab9f3b09b876236c70af08
saif43 <saif.ahmed.anik.0@gmail.com>
2020-02-14 12:50:57 +0600

    8.4

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -2,15 +2,17 @@ import React from "react";
 import axios from "axios";
 import Searchbar from "./SearchBar";
 class App extends React.Component {
-  onSearchSubmit(term){
-    axios.get('https://api.unsplash.com/search/photos', {
+  async onSearchSubmit(term){
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
       params: {
         query: term
       },
       headers : {
         Authorization: 'Client-ID 673f4cb9d1f284f6b56cbaffdafd1de413b44a3a0d82cca07a79d74bbf3eacb0'
       },
-    })
+    });
+
+    console.log(response.data.results);
   }
 
   render(){
