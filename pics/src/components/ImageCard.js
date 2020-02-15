41a44a87f9e383f73d9be6cc92fedc3068fff0e4
saif43 <saif.ahmed.anik.0@gmail.com>
2020-02-15 13:36:39 +0600

    10.7

--- pics/src/components/ImageCard.js
+++ pics/src/components/ImageCard.js
@@ -5,17 +5,27 @@ export default class ImageCard extends React.Component {
     super(props);
 
     this.imageRef = React.createRef();
+
+    this.state = { span: 0 };
   }
 
   componentDidMount() {
-    console.log(this.imageRef);
+    this.imageRef.current.addEventListener("load", this.setSpan);
   }
+
+  setSpan = () => {
+    const height = this.imageRef.current.clientHeight;
+
+    const span = Math.ceil(height / 10);
+    // console.log(span);
+    this.setState({ span });
+  };
 
   render() {
     const { description, urls } = this.props.image;
 
     return (
-      <div>
+      <div style={{ gridRowEnd: `span ${this.state.span}` }}>
         <img ref={this.imageRef} src={urls.regular} alt={description} />
       </div>
     );
