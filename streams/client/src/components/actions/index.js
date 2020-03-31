f2b859826d438cc87ec22e8a1f1b1518a10f9e55
saif43 <saif.ahmed.anik.0@gmail.com>
2020-03-31 18:07:20 +0600

    17.16

--- streams/client/src/components/actions/index.js
+++ streams/client/src/components/actions/index.js
@@ -1,11 +1,14 @@
-export const signIn = () => {
+import { SIGN_IN, SIGN_OUT } from "./../actions/types";
+
+export const signIn = userid => {
   return {
-    type: "SIGN_IN"
+    type: SIGN_IN,
+    payload: userid
   };
 };
 
 export const signOut = () => {
   return {
-    type: "SIGN_OUT"
+    type: SIGN_OUT
   };
 };
--- streams/client/src/components/reducers/authReducer.js
+++ streams/client/src/components/reducers/authReducer.js
@@ -1,14 +1,17 @@
+import { SIGN_IN, SIGN_OUT } from "./../actions/types";
+
 const INITIAL_STATE = {
-  isSignedIn: null
+  isSignedIn: null,
+  userid: null
 };
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case "SIGN_IN":
-      return { ...state, isSignedIn: true };
+    case SIGN_IN:
+      return { ...state, isSignedIn: true, userid: action.payload };
 
-    case "SIGN_OUT":
-      return { ...state, isSignedIn: false };
+    case SIGN_OUT:
+      return { ...state, isSignedIn: false, userid: null };
 
     default:
       return state;
