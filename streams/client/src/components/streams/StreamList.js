eb5ca343c991324f2827ced7b233f29ea94313cd
saif43 <saif.ahmed.anik.0@gmail.com>
2020-04-08 03:12:54 +0600

    22.8

--- streams/client/src/components/streams/StreamList.js
+++ streams/client/src/components/streams/StreamList.js
@@ -12,8 +12,15 @@ class StreamList extends React.Component {
     if (this.props.currentUserId === stream.userid) {
       return (
         <div className="right floated content">
-          <Link to={`/streams/edit/${stream.id}`} className="ui button primary">Edit</Link>
-          <Link to={`/streams/delete/${stream.id}`} className="ui button negative">Delete</Link>
+          <Link to={`/streams/edit/${stream.id}`} className="ui button primary">
+            Edit
+          </Link>
+          <Link
+            to={`/streams/delete/${stream.id}`}
+            className="ui button negative"
+          >
+            Delete
+          </Link>
         </div>
       );
     }
@@ -38,7 +45,7 @@ class StreamList extends React.Component {
           {this.renderAdminButtons(stream)}
           <i className="large middle aligned icon camera" />
           <div className="content">
-            {stream.title}
+            <Link to={`/streams/${stream.id}`}>{stream.title}</Link>
             <div className="description">{stream.description}</div>
           </div>
         </div>
--- streams/client/src/components/streams/StreamShow.js
+++ streams/client/src/components/streams/StreamShow.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchSingleStream } from "../../actions";
 
-const StreamShow = () => {
-  return <div>StreamShow</div>;
+class StreamShow extends Component {
+  componentDidMount() {
+    this.props.fetchSingleStream(this.props.match.params.id);
+  }
+
+  render() {
+    console.log(this.props);
+    return <div>StreamShow</div>;
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    stream: state.stream[ownProps.match.params.id],
+  };
 };
 
-export default StreamShow;
+export default connect(mapStateToProps, { fetchSingleStream })(StreamShow);
