0c012fa8657903a3369a4d905de9b37c90624b45
saif43 <saif.ahmed.anik.0@gmail.com>
2020-04-08 14:35:14 +0600

    22.11

--- streams/client/src/components/streams/StreamShow.js
+++ streams/client/src/components/streams/StreamShow.js
@@ -1,15 +1,54 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { fetchSingleStream } from "../../actions";
+import flv from "flv.js";
 
 class StreamShow extends Component {
+  constructor(props) {
+    super(props);
+
+    this.videoRef = React.createRef();
+  }
+
   componentDidMount() {
-    this.props.fetchSingleStream(this.props.match.params.id);
+    const { id } = this.props.match.params;
+    this.props.fetchSingleStream(id);
+
+    this.buildPlayer(id);
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer(this.props.match.params.id);
+  }
+
+  buildPlayer(id) {
+    if (this.player || !this.props.stream) {
+      return;
+    }
+
+    this.player = flv.createPlayer({
+      type: "flv",
+      url: `http://localhost:8000/live/${id}.flv`,
+    });
+
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
   }
 
   render() {
-    console.log(this.props);
-    return <div>StreamShow</div>;
+    if (this.props.stream) {
+      const { title, description } = this.props.stream;
+
+      return (
+        <div>
+          <video style={{ width: "100%" }} ref={this.videoRef} controls></video>
+          <h1>{title}</h1>
+          <h5>{description}</h5>
+        </div>
+      );
+    }
+
+    return <div>Loading</div>;
   }
 }
 
